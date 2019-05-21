bf7f71f0fb3bdf61c57b6c71cf095ac3e22a0b85
Tim Rach <tim.rach1987@gmail.com>
2019-05-21 14:36:34 +0200

    Added comments

--- js/Player.js
+++ js/Player.js
@@ -22,4 +22,23 @@ class Player {
     return tokens;
   }
 
+  /**
+  * Gets all tokens that havent been dropped.
+  * @return  {array}  Array of unused tokens.
+  */
+  get unusedTokens(){
+    return this.tokens.filter(token => !token.dropped);
+  }
+
+  /**
+  * Gets the active token by returning the first token in the array of unused tokens.
+  * @return  {Object}  First token object in the array of unused tokens.
+  */
+  get activeToken(){
+    return this.unusedTokens[0];
+  }
+
+
+
+
 }
