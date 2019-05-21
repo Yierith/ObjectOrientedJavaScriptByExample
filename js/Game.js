63d04ac4d9a849bb291d5606970dcf2263c8a0a5
Tim Rach <tim.rach1987@gmail.com>
2019-05-21 14:36:25 +0200

    Added comments

--- js/Game.js
+++ js/Game.js
@@ -5,14 +5,30 @@ class Game {
     this.ready = false;
   }
 
+  /**
+  * Returns active player.
+  * @return  {Object}    player - The active player.
+  */
   createPlayers(){
     const players = [new Player('Player 1', 1, '#e15258', true),
                       new Player('Player 2', 2, '#e59a13')];
     return players;
   }
 
+
+  /**
+  * Creates two player objects
+  * @return  {array}    An array of two player objects.
+  */
+  get activePlayer(){
+    return this.players.find( player => player.active );
+  }
+
+  /**
+  * Gets game ready for play
+  */
   startGame(){
-    
+
   }
 
 }
