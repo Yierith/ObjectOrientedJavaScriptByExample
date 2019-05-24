9ac01839e09e7445ebcabaddef945c18f330a11a
Tim Rach <tim.rach1987@gmail.com>
2019-05-24 11:01:46 +0200

    Added columnLocation moveLeft, moveRight and drop functions

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
     this.owner = owner;
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
+    this.columnLocation = 0;
   }
 
   /**
@@ -10,8 +11,16 @@ class Token {
   * @return  {element}   Html element associated with token object.
   */
   get htmlToken(){
-
+    return document.getElementById(this.id);
   }
+
+  /**
+   * Gets left offset of html element.
+   * @return  {number}   Left offset of token object's htmlToken.
+   */
+   get offsetLeft(){
+     return this.htmlToken.offsetLeft
+   }
 
   /**
   * Draws new HTML token.
@@ -23,5 +32,38 @@ class Token {
     token.setAttribute('class', 'token');
     token.style.background = this.owner.color;
   }
+  /**
+  * Moves html token one column to left.
+  */
+  moveLeft(){
+    if( this.columnLocation > 0 ){
+      this.htmlToken.style.left = this.offsetLeft - 76;
+      this.columnLocation -= 1;
+    }
+  }
+  /**
+  * Moves html token one column to right.
+  */
+  moveRight(columns){
+    if( this.columnLocation < columns - 1 ){
+      this.htmlToken.style.left = this.offsetLeft + 76;
+      this.columnLocation += 1;
+    }
+  }
+
+  /**
+ * Drops html token into targeted board space.
+ * @param   {Object}   target - Targeted space for dropped token.
+ * @param   {function} reset  - The reset function to call after the drop animation has completed.
+ */
+  drop(target, reset){
+    this.dropped = true;
+    
+    $(this.htmlToken).animate({
+        top: (target.y * target.diameter)
+    }, 750, 'easeOutBounce', reset);
+
+  }
+
 
 }
